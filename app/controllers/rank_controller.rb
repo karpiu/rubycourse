d6b315067871b4d3e4d65c4a89073e6458648870
michail <michailgames@yahoo.com>
2015-01-09 00:28:36 +0100

    [#8] added global refresh button

--- app/controllers/rank_controller.rb
+++ app/controllers/rank_controller.rb
@@ -4,4 +4,23 @@ class RankController < ApplicationController
     @milestones = Milestone.order('number ASC')
     @tasks = Task.order('number ASC')
   end
+
+  def refresh
+    @students = Student.all
+    @tasks = Task.all
+    @students.each do |student|
+      @tasks.each do |task|
+        student_task = student.student_tasks.find { |st| st.task == task }
+        if student_task.nil?
+          student_task = StudentTask.new
+          student_task.student = student
+          student_task.task = task
+        end
+        student_task.state = GithubHelper.check_issue_state(student, task, REPO_NAME)
+        student_task.save!
+      end
+    end
+
+    redirect_to root_path
+  end
 end

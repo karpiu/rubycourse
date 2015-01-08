3359cfab1dcff82b6a01f42680369b8c41230cba
michail <michailgames@yahoo.com>
2015-01-08 17:46:59 +0100

    [#6] optimized finding student tasks

--- app/controllers/rank_controller.rb
+++ app/controllers/rank_controller.rb
@@ -3,6 +3,5 @@ class RankController < ApplicationController
     @students = Student.order('id ASC')
     @milestones = Milestone.order('number ASC')
     @tasks = Task.order('number ASC')
-    @student_tasks = StudentTask.all
   end
 end

1dcea7b7283f3f44c93969ae21a9b8fe00952972
michail <michailgames@yahoo.com>
2015-01-09 21:27:12 +0100

    [#5] fixed wrong string comparisons in GithubHelper

--- lib/github_helper.rb
+++ lib/github_helper.rb
@@ -2,13 +2,13 @@ class GithubHelper
 
   def self.check_issue_state(student, task, repo_name)
     issue = Octokit.issue(student.github_login + "/" + repo_name, task.number)
-    if "closed".equal? issue.state
+    if "closed" == issue.state
       return "accepted"
     end
-    if issue.labels.find { |label| "Rejected".equal? label.name }
+    if issue.labels.find { |label| "rejected" == label.name.downcase }
       return "rejected"
     end
-    if issue.labels.find { |label| "Done".equal? label.name }
+    if issue.labels.find { |label| "done" == label.name.downcase }
       return "done"
     end
     if issue.assignee.nil?
